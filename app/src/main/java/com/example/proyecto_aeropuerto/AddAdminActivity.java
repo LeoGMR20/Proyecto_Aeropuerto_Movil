378dc60a38c59b2ca2c90be1a6a6b43e82011ad1
Rhory1 <rlimaticona@gmail.com>
2022-06-01 17:28:51 -0400

    Commit 01/06/2022 17:28 (Leandro Morales) -> Le añadí la pantalla de check In

--- app/src/main/java/com/example/proyecto_aeropuerto/AddAdminActivity.java
+++ app/src/main/java/com/example/proyecto_aeropuerto/AddAdminActivity.java
@@ -3,8 +3,10 @@ package com.example.proyecto_aeropuerto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddAdminActivity extends AppCompatActivity {
 
@@ -18,8 +20,51 @@ public class AddAdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_admin);
         inicializarVistas();
+        btnAddAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                obtenerInformacion();
+            }
+        });
     }
 
     private void inicializarVistas() {
+        etCI = findViewById(R.id.etCI);
+        etNombres = findViewById(R.id.etNombres);
+        etApPat = findViewById(R.id.etApPat);
+        etApMat = findViewById(R.id.etApMat);
+        etClave2 = findViewById(R.id.etClave2);
+        etConfirmarClave = findViewById(R.id.etConfirmarClave);
+        btnAddAdmin = findViewById(R.id.btnAddAdmin);
+    }
+
+    private void obtenerInformacion() {
+        if(etCI.getText().toString().isEmpty() ||
+                etNombres.getText().toString().isEmpty() ||
+                (etApPat.getText().toString().isEmpty() &&
+                        etApMat.getText().toString().isEmpty()) ||
+                etClave2.getText().toString().isEmpty() ||
+                etConfirmarClave.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Ingrese todos los datos", Toast.LENGTH_LONG).show();
+        }
+        else if(etCI.getText().toString().length() < 7){
+            Toast.makeText(this, "El CI ingresado es demasiado pequeño", Toast.LENGTH_LONG).show();
+        }
+        else if(etNombres.getText().toString().length() < 3){
+            Toast.makeText(this, "Nombre(s) ingresado(s) demasiado(s) pequeño(s)", Toast.LENGTH_LONG).show();
+        }
+        else if(etApPat.getText().toString().length() < 3 ||
+                etApMat.getText().toString().length() < 3) {
+            Toast.makeText(this, "Apellido Paterno o materno ingresados son demasiados pequeños", Toast.LENGTH_LONG).show();
+        }
+        else if(etClave2.getText().toString().length() < 8) {
+            Toast.makeText(this, "Contraseña ingresada demasiada pequeña", Toast.LENGTH_LONG).show();
+        }
+        else if(etConfirmarClave.getText().toString().equals(etClave2.getText().toString())) {
+            Toast.makeText(this, "El CI ingresado es demasiado pequeño", Toast.LENGTH_LONG).show();
+        }
+        else{
+
+        }
     }
 }
--- app/src/main/java/com/example/proyecto_aeropuerto/InicioSesionAdminActivity.java
+++ app/src/main/java/com/example/proyecto_aeropuerto/InicioSesionAdminActivity.java
@@ -5,24 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class InicioSesionAdminActivity extends AppCompatActivity {
 
     //Componentes visuales
 
     private TextView tvTituloSignIn;
-    private EditText etCodAdmin, etClave;
+    private EditText etCod, etClave;
     private Button btnIniciarSesion;
+    private Spinner spEmpleado, spAerolinea;
+
+    //Atributos
+
+    private ArrayList<String> opcionesEmpleado, opcionesAerolinea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion_admin);
         inicializarVistas();
+        popularSpinnerAerolinea();
+        popularSpinnerEmpleado();
         btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,13 +45,15 @@ public class InicioSesionAdminActivity extends AppCompatActivity {
 
     private void inicializarVistas() {
         tvTituloSignIn = findViewById(R.id.tvTitulo);
-        etCodAdmin = findViewById(R.id.etCodAdmin);
+        etCod = findViewById(R.id.etCod);
         etClave = findViewById(R.id.etClave);
         btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        spEmpleado = findViewById(R.id.spEmpleado);
+        spAerolinea = findViewById(R.id.spAerolinea);
     }
 
     private void obtenerInformacion() {
-        if(etCodAdmin.getText().toString().isEmpty() || etClave.getText().toString().isEmpty()){
+        if(etCod.getText().toString().isEmpty() || etClave.getText().toString().isEmpty()){
             Toast.makeText(this, "Ingrese todos los campos", Toast.LENGTH_LONG).show();
         }
         else{
@@ -50,5 +64,25 @@ public class InicioSesionAdminActivity extends AppCompatActivity {
     private void pasarOtraPantalla() {
         Intent intencionInicioSesionAdmin = new Intent(this,MenuAdminActivity.class);
         startActivity(intencionInicioSesionAdmin);
+    }
+
+    private void popularSpinnerEmpleado() {
+        opcionesEmpleado = new ArrayList<>(Arrays.asList("Tipo de empleado","Empleado abordaje","Administrador"));
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                opcionesEmpleado
+        );
+        spEmpleado.setAdapter(adaptador);
+    }
+
+    private void popularSpinnerAerolinea() {
+        opcionesAerolinea = new ArrayList<>(Arrays.asList("Aerolinea","BOA","Amazonas","Ecojet"));
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                opcionesAerolinea
+        );
+        spAerolinea.setAdapter(adaptador);
     }
 }
